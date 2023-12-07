7de5a40b939c21a2a1948cce6fad56fe4475d500
king-junho <junho087387@gmail.com>
2023-12-07 20:57:40 +0900

    [ADD] player Movement 구현
    Player 이동 구현

---
+++ Assets/Scripts/Controllers/PlayerInputController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownCharacterController
+{
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;   //마우스의 world좌표에서 player에 transform position 좌표를 빼고 평균화 시켜서 player로부터 마우스까지의 방향 벡터가 됨.
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+
+    public void OnFire(InputValue value)
+    {
+    }
+}
