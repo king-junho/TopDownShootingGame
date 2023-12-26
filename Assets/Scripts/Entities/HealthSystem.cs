6d3363e9707fb013bb6e70863bda9fbc9305ee71
king-junho <junho087387@gmail.com>
2023-12-26 19:54:01 +0900

    [Feature] Audio Manager add

--- Assets/Scripts/Entities/HealthSystem.cs
+++ Assets/Scripts/Entities/HealthSystem.cs
@@ -15,6 +15,8 @@ public class HealthSystem : MonoBehaviour
     public event Action OnDeath;
     public event Action OnInvincibilityEnd;
 
+    public AudioClip damageClip;
+
     public float CurrentHealth { get; private set; }
     public float MaxHealth => _statsHandler.CurrentStats.maxHealth;
     private void Awake()
@@ -54,6 +56,9 @@ public class HealthSystem : MonoBehaviour
         else
         {
             OnDamage?.Invoke();
+            
+            if(damageClip)
+                SoundManager.PlayClip(damageClip);
         }
         if(CurrentHealth <=0f)
         {
